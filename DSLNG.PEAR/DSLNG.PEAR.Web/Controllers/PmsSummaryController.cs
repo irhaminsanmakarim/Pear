a3ba1ac6a54817a74fea9f0dbff981654862da94
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-06-12 16:14:02 +0700

    add grouping on pms summary table

--- DSLNG.PEAR/DSLNG.PEAR.Web/Controllers/PmsSummaryController.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Controllers/PmsSummaryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DSLNG.PEAR.Web.ViewModels.PmsSummary;
 
 namespace DSLNG.PEAR.Web.Controllers
 {
@@ -10,7 +11,83 @@ namespace DSLNG.PEAR.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new PmsSummaryIndexViewModel();
+
+            viewModel.PmsSummaries = AddFakePmsSummaryData();
+            return View(viewModel);
+        }
+
+        public ActionResult IndexGridPartial()
+        {
+            return PartialView("_IndexGridPartial", AddFakePmsSummaryData());
+        }
+
+
+
+        private List<PmsSummaryViewModel> AddFakePmsSummaryData()
+        {
+            var list = new List<PmsSummaryViewModel>();
+            var pmsSummary1 = new PmsSummaryViewModel
+            {
+                Unit = 39,
+                Weight = 20,
+                ActualMonthly = 10,
+                ActualYearly = 20,
+                ActualYtd = 30,
+                IndexMonthly = 40,
+                IndexYearly = 59,
+                IndexYtd = 10,
+                Osp = "Safety",
+                PerformanceIndicator = "Fatality/Strap Disability",
+                Score = 20,
+                TargetMonthly = 10,
+                TargetYearly = 29,
+                TargetYtd = 20
+            };
+
+            var pmsSummary2 = new PmsSummaryViewModel
+            {
+                Unit = 390,
+                Weight = 100,
+                ActualMonthly = 210,
+                ActualYearly = 320,
+                ActualYtd = 340,
+                IndexMonthly = 240,
+                IndexYearly = 539,
+                IndexYtd = 104,
+                Osp = "Safety",
+                PerformanceIndicator = "RIF",
+                Score = 202,
+                TargetMonthly = 101,
+                TargetYearly = 22,
+                TargetYtd = 21
+            };
+
+            var pmsSummary3 = new PmsSummaryViewModel
+            {
+                Unit = 390,
+                Weight = 100,
+                ActualMonthly = 210,
+                ActualYearly = 320,
+                ActualYtd = 340,
+                IndexMonthly = 240,
+                IndexYearly = 539,
+                IndexYtd = 104,
+                Osp = "Productivity and Efficiency",
+                PerformanceIndicator = "RIF",
+                Score = 202,
+                TargetMonthly = 101,
+                TargetYearly = 22,
+                TargetYtd = 21
+            };
+
+
+
+            list.Add(pmsSummary1);
+            list.Add(pmsSummary2);
+            list.Add(pmsSummary3);
+            return list;
+
         }
 	}
 }
---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/PmsSummary/PmsSummaryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DSLNG.PEAR.Web.ViewModels.PmsSummary
+{
+    public class PmsSummaryViewModel
+    {
+        public string Osp { get; set; }
+        public string PerformanceIndicator { get; set; }
+        public decimal Unit { get; set; }
+        public decimal Weight { get; set; }
+
+        public decimal TargetYearly { get; set; }
+        public decimal TargetMonthly { get; set; }
+        public decimal TargetYtd { get; set; }
+
+        public decimal ActualYearly { get; set; }
+        public decimal ActualMonthly { get; set; }
+        public decimal ActualYtd { get; set; }
+
+        public decimal IndexYearly { get; set; }
+        public decimal IndexMonthly { get; set; }
+        public decimal IndexYtd { get; set; }
+
+        public decimal Score { get; set; }
+    }
+}
