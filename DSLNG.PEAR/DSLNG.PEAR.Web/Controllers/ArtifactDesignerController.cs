66c686f49e762129b9e9f4b6d0cba1cee6dd32af
unknown <cobian.wae@gmail.com>
2015-06-15 16:44:31 +0700

    artifact graph form

---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Controllers/ArtifactDesignerController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSLNG.PEAR.Web.Controllers
+{
+    public class ArtifactDesignerController : Controller
+    {
+        //
+        // GET: /ArtifactDesigner/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /ArtifactDesigner/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /ArtifactDesigner/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /ArtifactDesigner/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ArtifactDesigner/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ArtifactDesigner/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /ArtifactDesigner/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /ArtifactDesigner/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/ArtifactDesigner/ArtifactDesignerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSLNG.PEAR.Web.ViewModels.ArtifactDesigner
+{
+    public class ArtifactDesignerViewModel
+    {
+        public string GraphicType { get; set; }
+        public string GraphicName { get; set; }
+        public string HeaderTitle { get; set; }
+        public IEnumerable<SelectListItem> GraphicTypes { get; set; }
+    }
+}
