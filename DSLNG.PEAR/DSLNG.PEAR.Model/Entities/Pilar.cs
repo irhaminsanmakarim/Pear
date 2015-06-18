4f273da8180a22f75d1fc07b6220fb129804f74a
bagusdk <bagusdk@gmail.com>
2015-06-18 08:56:12 +0700

    Change Oder to Order

--- DSLNG.PEAR/DSLNG.PEAR.Model/Entities/Pilar.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Model/Entities/Pilar.cs
@@ -10,7 +10,7 @@ namespace DSLNG.PEAR.Data.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
-        public int Oder { get; set; }
+        public int Order { get; set; }
         public string Color { get; set; }
         public string Icon { get; set; }
         public string Remark { get; set; }
