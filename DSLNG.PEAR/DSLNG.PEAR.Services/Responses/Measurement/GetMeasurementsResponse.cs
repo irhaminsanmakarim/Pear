49e39e04a7e47797faf4ea69ea4780fcd0a5c0fd
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-06-19 15:21:46 +0700

    finishing master measurement

---
+++ DSLNG.PEAR/DSLNG.PEAR.Services/Responses/Measurement/GetMeasurementsResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSLNG.PEAR.Services.Responses.Measurement
+{
+    public class GetMeasurementsResponse : BaseResponse
+    {
+        public IList<Measurement> Measurements { get; set; }
+
+        public class Measurement
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string IsActive { get; set; }
+            public string Remark { get; set; }
+        }
+    }
+}
