d1d1330ab75aa9e9e0eb3f44015c47e038e50b55
marwansaleh <marwan.saleh@regawa.com>
2015-06-22 15:18:48 +0700

    RoleGroup

---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/RoleGroup/RoleGroupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DSLNG.PEAR.Web.ViewModels.RoleGroup
+{
+    public class RoleGroupViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Remark { get; set; }
+    }
+}
