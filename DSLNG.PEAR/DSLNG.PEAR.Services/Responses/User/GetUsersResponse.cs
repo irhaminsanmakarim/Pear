7d4a92f284216bfe0d4020736aa1480a2dab4f44
marwansaleh <marwan.saleh@regawa.com>
2015-06-22 12:25:35 +0700

    .

--- DSLNG.PEAR/DSLNG.PEAR.Services/Responses/User/GetUsersResponse.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Services/Responses/User/GetUsersResponse.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DSLNG.PEAR.Services.Responses.User
 {
     public class GetUsersResponse : BaseResponse
     {
-        public IList<UserResponse> Users { get; set; }
+        public IList<User> Users { get; set; }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+        }
     }
 }
