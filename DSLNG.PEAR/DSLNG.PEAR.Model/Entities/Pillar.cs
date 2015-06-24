0c8969fa97bc0debe5f4738f926d290b5b038e38
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-06-24 14:10:05 +0700

    change filename

---
+++ DSLNG.PEAR/DSLNG.PEAR.Model/Entities/Pillar.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DSLNG.PEAR.Data.Entities
+{
+    public class Pillar
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Order { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+        public string Remark { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
