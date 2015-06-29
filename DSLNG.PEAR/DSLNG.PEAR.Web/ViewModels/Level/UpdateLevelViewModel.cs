5b37a3d8c12d8703f802e134cfbd4f6f57288131
marwansaleh <marwan.saleh@regawa.com>
2015-06-29 09:44:43 +0700

    Level view model

---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/Level/UpdateLevelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DSLNG.PEAR.Web.ViewModels.Level
+{
+    public class UpdateLevelViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Order")]
+        public int Number { get; set; }
+        public string Remark { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
