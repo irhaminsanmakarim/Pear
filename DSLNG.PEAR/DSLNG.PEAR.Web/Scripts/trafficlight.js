030fea675fbb1d6526547907eebeca7434819146
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-07-23 05:21:43 +0700

    traficlight chart

--- DSLNG.PEAR/DSLNG.PEAR.Web/Scripts/trafficlight.js
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Scripts/trafficlight.js
@@ -1,14 +1,23 @@
 ﻿(function ($) {
     $.fn.trafficlight = function (options) {
-        /*var settings = $.extend({
-        }, options);*/
-        debugger;
         this.html('');
         var wrapper = $('<div/>', { 'class': 'trafficlight-wrapper' });
         var data = options.Series.data[0];
+        
+        console.log(this);
+        console.log($(this));
+        var containerHeight = this.height();
+        console.log(containerHeight);
+        var circleHeight = containerHeight / (options.PlotBands.length + 3);
+        var itemHeight = circleHeight + 40;
+        var itemWidth = circleHeight + 20;
+        console.log(circleHeight);
+        console.log(itemHeight);
+        console.log(itemWidth);
+        
         for (var i = 0; i < options.PlotBands.length; i++) {
-            var trafficLightItem = $('<div/>', { 'class': 'trafficlight-item' });
-            var traffictLightCircle = $('<div/>', { 'class': 'trafficlight-circle', style: 'background-color:' + options.PlotBands[i].color });
+            var trafficLightItem = $('<div/>', { 'class': 'trafficlight-item', 'style': 'height:' + itemHeight + 'px;width:' + itemHeight + 'px' });
+            var traffictLightCircle = $('<div/>', { 'class': 'trafficlight-circle', style: 'background-color:' + options.PlotBands[i].color + ';height:' + circleHeight + 'px;width:' + circleHeight + 'px' });
             if ((options.PlotBands[i].from <= data) && (data <= options.PlotBands[i].to)) {
                 traffictLightCircle.css('opacity', '1');
                 trafficLightItem.append('<span class="trafficlight-label">' + options.PlotBands[i].label + '</span>');
@@ -16,7 +25,13 @@
             trafficLightItem.append(traffictLightCircle);
             wrapper.append(trafficLightItem);
         }
-
+        
+        
+        /*$('.trafficlight-item').width(itemWidth);
+        $('.trafficlight-circle').height(circleHeight);
+        $('.trafficlight-circle').width(circleHeight);
+        */
+        this.append('<span style="font-size:16px">' + options.Title + '</span>');
         this.append(wrapper);
         return this;
     };
