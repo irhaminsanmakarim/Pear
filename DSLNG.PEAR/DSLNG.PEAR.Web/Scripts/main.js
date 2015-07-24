3d4f8777433833522521490cd68098bb3865ce29
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-07-24 08:21:50 +0700

    add preview

--- DSLNG.PEAR/DSLNG.PEAR.Web/Scripts/main.js
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Scripts/main.js
@@ -1530,7 +1530,7 @@ String.prototype.isNullOrEmpty = function () {
                 newColumn.css('width', newWidth + '%');
                 Pear.Template.Editor._artifactSelectField(newColumn);
                 $('<input>').attr({
-                    type: 'text',
+                    type: 'hidden',
                     id: 'foo',
                     name: 'LayoutRows[' + $row.data('row-pos') + '].LayoutColumns.Index',
                     value: columnCount
@@ -1585,6 +1585,28 @@ String.prototype.isNullOrEmpty = function () {
                 $(val).css('width', newWidth + '%');
             });
         });
+        
+        $('#graphic-preview-btn').click(function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            $.ajax({
+                url: $this.data('preview-url'),
+                data: $this.closest('form').serialize(),
+                method: 'POST',
+                success: function (data) {
+                    $('#container').html(data);
+                    templateEditor.ViewSetup();
+                    $('#graphic-preview').modal('show');
+                }
+            });
+        });
+        $('#graphic-preview').on('show.bs.modal', function () {
+            $('#container').css('visibility', 'hidden');
+        });
+        $('#graphic-preview').on('shown.bs.modal', function () {
+            $('#container').css('visibility', 'initial');
+        });
+        
         addRow();
         addColumn();
     };
@@ -1674,6 +1696,27 @@ String.prototype.isNullOrEmpty = function () {
             $row.children('.layout-column').each(function (i, val) {
                 $(val).css('width', newWidth + '%');
             });
+        });
+        
+        $('#graphic-preview-btn').click(function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            $.ajax({
+                url: $this.data('preview-url'),
+                data: $this.closest('form').serialize(),
+                method: 'POST',
+                success: function (data) {
+                    $('#container').html(data);
+                    templateEditor.ViewSetup();
+                    $('#graphic-preview').modal('show');
+                }
+            });
+        });
+        $('#graphic-preview').on('show.bs.modal', function () {
+            $('#container').css('visibility', 'hidden');
+        });
+        $('#graphic-preview').on('shown.bs.modal', function () {
+            $('#container').css('visibility', 'initial');
         });
         
         addRow();
