71572a409b8de812ee0be23dac4d5fe9588be97a
bagusdk <bagusdk@gmail.com>
2015-07-30 11:20:26 +0700

    Update Pms Config Detail

--- DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/PmsConfigDetails/UpdatePmsConfigDetailsViewModel.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/PmsConfigDetails/UpdatePmsConfigDetailsViewModel.cs
@@ -7,13 +7,38 @@ namespace DSLNG.PEAR.Web.ViewModels.PmsConfigDetails
     public class UpdatePmsConfigDetailsViewModel
     {
         public int Id { get; set; }
+        public int PmsConfigId { get; set; }
         public int PillarId { get; set; }
         public string PillarName { get; set; }
         public int KpiId { get; set; }
+        public Kpi Kpi { get; set; }
         public string KpiName { get; set; }
         public double Weight { get; set; }
         public string ScoringType { get; set; }
         public IEnumerable<SelectListItem> ScoringTypes { get; set; }
         public IEnumerable<ScoreIndicatorViewModel> ScoreIndicators { get; set; }
+
+        public IEnumerable<SelectListItem> Kpis { get; set; }
+    }
+
+    public class Kpi
+    {
+        public int Id { get; set; }
+        public Pillar Pillar { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string Remark { get; set; }
+    }
+
+    public class Pillar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Order { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+        public string Remark { get; set; }
+        public bool IsActive { get; set; }
     }
 }
