4ab7f47b2ff5a590ebc372640aa8d7898e17f3a7
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-08-03 16:48:33 +0700

    bug fixing : format number on pms summary and pms detail

---
+++ DSLNG.PEAR/DSLNG.PEAR.Common/Contants/FormatNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSLNG.PEAR.Common.Contants
+{
+    public static class FormatNumber
+    {
+        public const string DecimalFormat = "#,##0.##";
+    }
+}
