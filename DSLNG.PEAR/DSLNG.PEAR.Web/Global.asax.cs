323b966aea0fa37198f9b6f93704bdff42920597
bagusdk <bagusdk@gmail.com>
2015-08-19 11:21:02 +0700

    disable migration from EF

--- DSLNG.PEAR/DSLNG.PEAR.Web/Global.asax.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Global.asax.cs
@@ -26,7 +26,8 @@ namespace DSLNG.PEAR.Web
         protected void Application_Start()
         {
             AutoMapperConfiguration.Configure();
-            Database.SetInitializer<DataContext>(new DataInitializer());
+            //Database.SetInitializer<DataContext>(new DataInitializer());
+            Database.SetInitializer<DataContext>(null);
             
             //StructureMap Container
             IContainer container = IoC.Initialize();
